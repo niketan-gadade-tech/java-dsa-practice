9e1dcb2901924eb7f1a0c9001bded4c064e5e4c5
Niketan Gadade <niketangadade88@gmail.com>
2026-09-14 20:28:52 +0530

    day-02

---
+++ basics/day_01_Datatype_basic/Program1.java
@@ -0,0 +1,7 @@
+/// Q1.A student is in class 11th and there are 12 divisions of the class which data type is feasible to print the class and divisions.
+
+class Program1{
+	public static void main(String[] args){
+
+	}
+}
